2d28d5a48815ac77af2fd27cc47bc3d888bb2eed
albert ou <albert_ou@epam.com>
2021-09-07 11:06:06 +0800

    feign-client : init

--- exec-test.cmd
+++ exec-test.cmd
@@ -4,8 +4,8 @@ curl http://localhost:8088/another-id
 
 curl http://localhost:8088/another-id
 
-curl http://localhost:8088/another-id
+curl http://localhost:8088/circuit-id
 
-curl http://localhost:8088/another-id
+curl http://localhost:8088/retry-id
 
 
