cd5f4edf6f20c98e02bb6c8b1ed98664450f65d6
albert ou <albert_ou@epam.com>
2021-09-23 16:16:33 +0800

    restTemplate exchange()

--- discovery-client-lb/src/main/java/com/example/demo/controller/SimpleController.java
+++ discovery-client-lb/src/main/java/com/example/demo/controller/SimpleController.java
@@ -3,16 +3,18 @@ package com.example.demo.controller;
 import com.example.demo.data.MyData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.naming.ServiceUnavailableException;
+import java.net.URI;
 
 @Slf4j
 @RestController
-public class AnotherClientController {
+public class SimpleController {
 
     @Autowired
     MyData data ;
@@ -37,4 +39,17 @@ public class AnotherClientController {
         return data;
     }
 
+    /**
+     *
+    void test(){
+        restTemplate
+                .exchange(
+                        RequestEntity
+                                .post("http://abbbb/id")
+                                .header("Ce-type", "open-type")
+                                .body("hello"),
+                        String.class )
+                ;
+    }
+    */
 }
